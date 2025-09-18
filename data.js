d63b962762f1f38b1d92d18710eb4bcb6de0c5e0
Eliana. <e.arteinsana@gmail.com>
2025-09-18 14:03:40 +0200

    📸 images mock up

--- data.js
+++ data.js
@@ -4,6 +4,33 @@ import SING from "./assets/Sing.svg";
 import GAME from "./assets/Game.svg";
 import THE_KISS from "./assets/Kiss.svg";
 
+import DANSE_AGENDA from "./assets/produits/danse-agenda.jpg";
+import DRESS_AGENDA from "./assets/produits/dress-agenda.jpg";
+import GAME_AGENDA from "./assets/produits/game-agenda.jpg";
+import KISS_AGENDA from "./assets/produits/kiss-agenda.jpg";
+import SING_AGENDA from "./assets/produits/sing-agenda.jpg";
+
+//T-SHIRTS BLANCS
+import DANSE_BLANC from "./assets/produits/danse-blanc.jpg";
+import DRESS_BLANC from "./assets/produits/danse-blanc.jpg";
+import GAME_BLANC from "./assets/produits/danse-blanc.jpg";
+import KISS_BLANC from "./assets/produits/danse-blanc.jpg";
+import SING_BLANC from "./assets/produits/danse-blanc.jpg";
+
+//T-SHIRTS NOIRS
+import DANSE_NOIR from "./assets/produits/danse-noir.jpg";
+import DRESS_NOIR from "./assets/produits/danse-noir.jpg";
+import GAME_NOIR from "./assets/produits/danse-noir.jpg";
+import KISS_NOIR from "./assets/produits/danse-noir.jpg";
+import SING_NOIR from "./assets/produits/danse-noir.jpg";
+
+//T-SHIRTS JAUNES
+import DANSE_JAUNE from "./assets/produits/danse-jaune.jpg";
+import DRESS_JAUNE from "./assets/produits/danse-jaune.jpg";
+import GAME_JAUNE from "./assets/produits/danse-jaune.jpg";
+import KISS_JAUNE from "./assets/produits/danse-jaune.jpg";
+import SING_JAUNE from "./assets/produits/danse-jaune.jpg";
+
 const products = [
   {
     id: 1,
@@ -57,14 +84,30 @@ const supports = {
 
 const variants = {
   tshirt: {
-    Noir: ["lien de l'image"],
-    Blanc: ["lien de l'image"],
-    Jaune: ["lien de l'image"],
+    Noir: ["DANSE_NOIR", "DRESS_NOIR", "GAME_NOIR", "KISS_NOIR", "SING_NOIR"],
+    Blanc: [
+      "DANSE_BLANC",
+      "DRESS_BLANC",
+      "GAME_BLANC",
+      "KISS_BLANC",
+      "SING_BLANC",
+    ],
+    Jaune: [
+      "DANSE_JAUNE",
+      "DRESS_JAUNE",
+      "GAME_JAUNE",
+      "KISS_JAUNE",
+      "SING_JAUNE",
+    ],
   },
   agenda: {
-    Bleu: ["lien de l'image"],
-    Blanc: ["lien de l'image"],
-    Jaune: ["lien de l'image"],
+    design: [
+      "DANSE_AGENDA",
+      "DRESS_AGENDA",
+      "GAME_AGENDA",
+      "KISS_AGENDA",
+      "SING_AGENDA",
+    ],
   },
 };
 
